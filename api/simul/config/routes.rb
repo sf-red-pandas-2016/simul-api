0850fcdb7daf991e2c9562bb8c6422b777bf3762
hypatiah <danielle.hassid@gmail.com>
2016-09-02 16:20:15 -0700

    Create users controller

--- api/simul/config/routes.rb
+++ api/simul/config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'users_controller/new'
+
+  get 'users_controller/create'
+
+  get 'users_controller/update'
+
+  get 'users_controller/edit'
+
+  get 'users_controller/destroy'
+
+  get 'users_controller/index'
+
+  get 'users_controller/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
