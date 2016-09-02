aafa9a04cd0713fe3f078dcf60158b1868438105
hypatiah <danielle.hassid@gmail.com>
2016-09-02 16:22:13 -0700

    Create Messages controller

---
+++ api/simul/app/controllers/messages_controller_controller.rb
@@ -0,0 +1,16 @@
+class MessagesControllerController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- api/simul/config/routes.rb
+++ api/simul/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'messages_controller/new'
+
+  get 'messages_controller/create'
+
+  get 'messages_controller/destroy'
+
+  get 'messages_controller/index'
+
+  get 'messages_controller/show'
+
   get 'users_controller/new'
 
   get 'users_controller/create'
