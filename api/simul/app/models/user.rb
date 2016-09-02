a8dcd0d2595f14f3d01c336ce41dd7248983bec5
hypatiah <danielle.hassid@gmail.com>
2016-09-02 15:55:42 -0700

    Add validations to User model

--- api/simul/app/models/user.rb
+++ api/simul/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  validates :name, :username, :location, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }
 end
