a598e67085ef210ea7baf68f96ecdb0d330ed762
hypatiah <danielle.hassid@gmail.com>
2016-09-02 20:19:24 -0700

    Create User.new_token method

--- api/simul/app/models/user.rb
+++ api/simul/app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :stories
-  
+
   validates :name, :username, :location, presence: true
   validates :username, uniqueness: true
   # validates :password, length: { minimum: 6 }
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
 
 end
