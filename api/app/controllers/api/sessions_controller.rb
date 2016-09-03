5b3d572038b8633eb1993447b6ccaf0ccad336b3
hypatiah <danielle.hassid@gmail.com>
2016-09-02 23:06:39 -0700

    Add create route Sessions Controller

--- api/app/controllers/api/sessions_controller.rb
+++ api/app/controllers/api/sessions_controller.rb
@@ -2,6 +2,13 @@ class Api::SessionsController < ApplicationController
   def new
   end
 
+  #If user login info is valid, return access token
   def create
+    user = User.find_by(username: params[:session][:username])
+      if user && user.authenticate(params[:session][:password])
+        render json: { text: user.access_token }, status: 200
+      else
+        render json: { text: "Username and password combination are invalid"}, status: 422
+      end
   end
 end
