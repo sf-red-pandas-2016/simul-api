b1263fc65212fb68fec9290c5a74bd18bcf720c8
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-03 15:04:29 -0700

    Create user for rspec testing

--- back-end/spec/models/user_spec.rb
+++ back-end/spec/models/user_spec.rb
@@ -1,1 +1,16 @@
 require 'rails-helper'
+
+describe User do
+  context
+  let(:user) { User.new(name: 'Oshoke',
+                        username: 'omaiki',
+                        location: 'San Francisco, CA'
+                        bio: 'no kids'
+                        preferred_contact: 'me@me.com'
+                        skills: 'coding'
+                        seeking: 'coding opportunities'
+                      }
+end
+
+
+
