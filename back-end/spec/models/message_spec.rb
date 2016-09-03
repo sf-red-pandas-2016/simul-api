1b8c84341ded7411c81358253af7da7df72002ba
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-03 16:45:22 -0700

    Add Message model test setup

--- back-end/spec/models/message_spec.rb
+++ back-end/spec/models/message_spec.rb
@@ -1,2 +1,13 @@
 require 'rails_helper'
 require 'spec_helper'
+
+describe Message do
+  let(:message) { Message.new(user_id: rand(1..10),
+                              author: 'Justin',
+                              author_contact: 'Danielle',
+                              subject: 'whoknows',
+                              content: 'nobody',)
+                            }
+  let(:invalid_message) { Message.new(user_id: rand(1..10)}
+
+end
