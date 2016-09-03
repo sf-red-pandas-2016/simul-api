a63efbc720eb32ac9501f0c690d77f1337255975
hypatiah <danielle.hassid@gmail.com>
2016-09-02 17:24:20 -0700

    Add index and create routes to Users controller

--- api/simul/app/controllers/api/users_controller_controller.rb
+++ api/simul/app/controllers/api/users_controller_controller.rb
@@ -1,9 +1,24 @@
 class UsersControllerController < ApplicationController
 
+  def index
+    users = User.all
+    render json: { users: users }
+  end
+
   def new
   end
 
   def create
+    user = User.create
+    if user.save
+      login user
+      redirect_to user
+    elsif
+      render json: { error: user.errors.full_messages }
+    end
+  end
+
+  def show
   end
 
   def update
@@ -15,9 +30,5 @@ class UsersControllerController < ApplicationController
   def destroy
   end
 
-  def index
-  end
 
-  def show
-  end
 end
---
+++ api/simul/db/schema.rb
@@ -0,0 +1,56 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160902222529) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "author",         null: false
+    t.string   "author_contact", null: false
+    t.string   "subject",        null: false
+    t.text     "content",        null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "stories", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title",      null: false
+    t.text     "content",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",                          null: false
+    t.string   "name",                              null: false
+    t.string   "location",                          null: false
+    t.text     "bio"
+    t.string   "resource_request"
+    t.string   "skills"
+    t.string   "seeking"
+    t.string   "preferred_contact"
+    t.boolean  "admin",             default: false
+    t.string   "password_digest"
+    t.string   "remember_digest"
+    t.string   "access_token"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.index ["access_token"], name: "index_users_on_access_token", using: :btree
+  end
+
+end
