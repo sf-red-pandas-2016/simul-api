6c6295ca076f8f450e7bd16f326e8445f04dff83
hypatiah <danielle.hassid@gmail.com>
2016-09-03 12:08:41 -0700

    Remove session routes, add welcome root

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-
   namespace :api do
-    post   'login'   => 'sessions#create'
-    delete 'logout'  => 'sessions#destroy'
-    get    'verify'  => 'sessions#verify_access_token'
+    root 'welcome#index'
+    resources :welcome, only: :index
     resources :users do
       resources :stories
       resources :messages, except: [:update, :edit]
     end
+    # post   'login'   => 'sessions#create'
+    # delete 'logout'  => 'sessions#destroy'
+    # get    'verify'  => 'sessions#verify_access_token'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
