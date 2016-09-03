e5e2bdfc715735ff789927fc17e26cfbf3ba587a
Nicolae Nitescu <nitescun@yahoo.com>
2016-09-02 20:46:24 -0700

    Add messages controller

--- api/simul/app/controllers/api/messages_controller.rb
+++ api/simul/app/controllers/api/messages_controller.rb
@@ -1,17 +1,34 @@
 class MessagesControllerController < ApplicationController
 
+  def index
+    messages = Message.where(user_id: params[:user_id])
+    render json: { messages: messages }
+  end
+
   def new
   end
 
   def create
+    message = Message.create
+    if message.save
+      login message
+      redirect_to message
+    else
+      render json: { error: message.errors.full_messages }
+    end
   end
 
   def destroy
   end
 
-  def index
-  end
 
   def show
+    if user.id == current_user.id
+      message = Message.find(params[:id])
+    # if message == current_message
+      render json: { message: message }
+    else
+      render json: { error: message.errors.full_messages }
+    end
   end
 end
