06b766aade67242470571e2e63fa0dadda4dd5d6
hypatiah <danielle.hassid@gmail.com>
2016-09-02 21:04:37 -0700

    Add preliminary generate token methods to User model

--- api/simul/app/models/user.rb
+++ api/simul/app/models/user.rb
@@ -2,13 +2,20 @@ class User < ApplicationRecord
   has_many :messages
   has_many :stories
 
+  before_create :generate_access_token
+
   validates :name, :username, :location, presence: true
   validates :username, uniqueness: true
   # validates :password, length: { minimum: 6 }
 
+  # Creates random token
   def User.new_token
     SecureRandom.urlsafe_base64
   end
 
+  private
+    def generate_access_token
+      self.access_token = User.new_token
+    end
 
 end
