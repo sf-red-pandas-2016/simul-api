389ef12b2e29622f8e83620aaf49a3c5b9bcec01
Oshoke Maiki <oshokemaiki@gmail.com>
2016-09-06 20:49:47 -0700

    Fix seed file duplicates, decrease number of Model.create to 30

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,18 @@ require 'ffaker'
 
 10.times do
   User.create(
-    name: FFaker::Name.name
-    username: FFaker::Name.user_name
-    location: FFaker::AddressUS.continental_state
-    bio: FFaker::HipsterIpsum.phrase
-    preferred_contact: FFaker::Internet.safe_email
-    resource_request: FFaker::Vehicle.fuel_type
-    skills: FFaker::Skill.specialties
     name: FFaker::Name.name,
     username: FFaker::Internet.user_name,
     location: FFaker::AddressUS.continental_state,
     bio: FFaker::HipsterIpsum.phrase,
     preferred_contact: FFaker::Internet.safe_email,
     skills: FFaker::Skill.specialties,
-    seeking: FFaker::Job.title
+    seeking: FFaker::Job.title,
+    resource_request: FFaker::Vehicle.fuel_type,
   )
 end
 
-50.times do
+30.times do
   Story.create(
     user_id: rand(1..10),
     title: FFaker::HipsterIpsum.phrase,
@@ -35,7 +29,7 @@ end
   )
 end
 
-50.times do
+30.times do
   Message.create(
     user_id: rand(1..10),
     author: FFaker::Name.name,
