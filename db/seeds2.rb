0e4636c848621dc3ab457d82015b2fa518426be5
hypatiah <danielle.hassid@gmail.com>
2016-09-07 22:58:41 -0700

    Add 3 users with stories

--- db/seeds2.rb
+++ db/seeds2.rb
@@ -34,3 +34,67 @@ Message.create(
   subject: "I have lots of milk cartons",
   content: "Hi Adnan, I have tons of milk cartons laying around from Mason Bottle. I'd be happy to give them to you and your brother. Email me back and we can get things set up!",
 )
+
+#2
+User.create(
+  name: "Mohammed",
+  username: "papa13",
+  location: "Istanbul, Turkey",
+  bio: "I love my children. They are my everything.",
+  preferred_contact: "dinosaur_king@gmail.com",
+  skills: "Repairing, operation monitoring, systems analysis.",
+  seeking: "Employment opportunities in enterprise operations.",
+  resource_request: "Resume writing in English and puzzles/toys for my kids.",
+  photo: 'https://static6.businessinsider.com/image/566ee7822340f838008b5601-1200/istanbul-turkey.jpg',
+)
+
+Story.create(
+  user_id: 2,
+  title: "The whole world in my hands",
+  content: "I was at home when the telephone rang. It was my mother. She told me that there had been a bomb at the boys' school. I immediately tried to call the school, but nobody answered. Then I tried to call the bus driver but he didn't answer either. I imagined the worst. The roads were closed, so I couldn’t get to the school. All I could do was pace around the house. Finally the bus driver answered the phone and said that everyone was alive. The bomb had landed on the playground and only destroyed one wall of the school. After several hours the roads were reopened, and they came back home. When I hugged them, it felt like the whole world was in my hands.",
+  photo: 'https://static6.businessinsider.com/image/566ee7822340f838008b5601-1200/istanbul-turkey.jpg',
+)
+
+Message.create(
+  user_id: 2,
+  author: "Leia Ferrari",
+  author_contact: "leia@devbootcamp.com",
+  subject: "Career advisor offering resume help",
+  content: "Hi Mohammed, I am a professional career advisor and I'd be happy to help polish your resume. Email me what you have as of now and we can get things set up!",
+)
+
+Message.create(
+  user_id: 2,
+  author: "Ken Rettberg",
+  author_contact: "ken.rettberg@devbootcamp.com",
+  subject: "I like puzzles and have lots of them",
+  content: "Hi Mohammed, I do puzzles all day and since I just got married to another puzzle master I have many duplicates I'd love to give to you and your kids. Write me back at the email above and I can send you them right away. Thanks. -Ken",
+)
+
+Message.create(
+  user_id: 2,
+  author: "Tolsen Wilmot",
+  author_contact: "tolsenwilmot@gmail.com",
+  subject: "Many toys for your kids",
+  content: "Mohammed, I have really cool toys I'd love to give you. Wrtie me an email and we can figure out which ones your kids will enjoy.",
+)
+
+#3
+User.create(
+  name: "Houmam",
+  username: "climber10",
+  location: "Istanbul, Turkey",
+  bio: "Moving to Clearwater, Florida soon.",
+  preferred_contact: "tree_climber@gmail.com",
+  skills: "Experience with farm animals and riding horses.",
+  seeking: "",
+  resource_request: "English tutoring",
+  photo: 'https://static4.businessinsider.com/image/566ee7822340f8b0008b55cf-1200/istanbul-turkey.jpg',
+)
+
+Story.create(
+  user_id: 3,
+  title: "The whole world in my hands",
+  content: "We are going to a place called Clearwater, Florida. I don't know a lot about it. I saw Florida on the television and it looks like it's close to the sea and has a lot of plants. My dad says the people are friendly and there are a lot of friendly kids there. I really hope that we can have a small farm and a horse when we get there because my grandmother really loves animals. I'd like it to be a square farm with lots of flowers and rabbits. I also hope there is a good tree in Florida because I'd like to build a tree house where we can have some adventures.",
+  photo:'https://static6.businessinsider.com/image/566ee7822340f838008b5601-1200/istanbul-turkey.jpg'
+)
